6890e64eec9b2d12cc8e4663c74d0a20ef33880b
Acosta <jhusdanacostahuaman28@gmail.com>
2020-09-15 22:22:13 -0500

    en mi formulario agregar productos se agrega botoncitos para poder modeficar y eliminar

--- modulos/agregar_producto.php
+++ modulos/agregar_producto.php
@@ -17,6 +17,12 @@ if(isset($enviar)){
     redir("?p=agregar_producto");
 }
 
+//Eliminar producto
+if(isset($eliminar)){
+    $mysqli->query("DELETE FROM productos WHERE id='$eliminar'");
+    alert("Se elimino producto");
+    redir("?p=agregar_producto");
+}
 ?>
 
 <form method="post"action="" enctype="multipart/form-data">
@@ -33,9 +39,62 @@ if(isset($enviar)){
     <input class="form-control" type="file" name="imagen" title="Imagen del Producto" placeholder="Imagen del producto">
     </div>
 
+        <div class="form-group">
+            <select class="form-control" name="categoria" required>
+            <option value="">Seleccione una categoria</option>
+                <?php
+                    $q=$mysqli->query("SELECT * FROM categorias ORDER BY categoria ASC");
+                    while($r=mysqli_fetch_array($q)){
+                        ?>
+                            <option value="<?=$r['id']?>"><?=$r['categoria']?></option>
+                        <?php
+                    }
+                ?>
+            </select>
+        </div>
 
     <div class="form-group">
     <button class="btn btn_succes" type="submit" name="enviar"><i class="fa fa-check"></i>Agregar Producto</button>
     </div>
 
 </form>
+<br>
+<br>
+<table class="table table-striped">
+        <tr>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Imagen</th>
+            <th>Categoria</th>
+            <th>Acciones</th>
+        </tr>
+
+
+        <?php
+            $prod=$mysqli->query("SELECT * FROM productos  ORDER BY id DESC");
+                 while($rp=mysqli_fetch_array($prod)){
+                     $cat=$mysqli->query("SELECT * FROM categorias WHERE id='".$rp['id_categoria']."'");
+                        if(mysqli_num_rows($cat)>0){
+                            $rcat=mysqli_fetch_array($cat);
+                            $categoria=$rcat['categoria'];
+                        }else{
+                            $categoria="--";       
+                 }
+                
+                ?>
+                    <tr>
+                        <td><?=$rp['names']?></td>
+                        <td><?=$rp['price']?></td>
+                        <td><img class="imagen_carro" src="productos/<?=$rp['imagen']?>" alt=""></td>
+                        <td><?=$categoria?></td>
+
+                        <td>
+                            <a href="?p=modeficar_producto&id=<?=$rp['id']?>"><i class="fa fa-edit"></i></a>
+                            &nbsp;
+                            <a href="?p=agregar_producto&eliminar=<?=$rp['id']?>"><i class="fa fa-times"></i></a>
+                        </td>
+                    </tr>
+                <?php
+            }
+        ?>
+</table>
